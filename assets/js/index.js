43351ac9a6a6d213edca7206bd6eb3c672cb4ab8
yinhan <495552532@qq.com>
2017-09-22 17:00:28 +0800

    remove lazyloading

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ $(function() {
         var dateArray = ["2017-08-28", "2017-09-01", "2017-09-03", "2017-09-09", "2017-09-10", "2017-09-16", "2017-09-17"];
         for (var i = 0, len = dateArray.length; i < len; i++) {
             var date = dateArray[i];
-            $swiperWrap.append('<div class="swiper-slide"><img data-src="' + qiniuBaseUrl + date + '.jpg" class="swiper-lazy" width="100%"><p class="swiper-desc">' + date + '</p></div>')
+            $swiperWrap.append('<div class="swiper-slide"><img src="' + qiniuBaseUrl + date + '.jpg" class="swiper-lazy" width="100%"><p class="swiper-desc">' + date + '</p></div>')
         }
     }
     initPic();
