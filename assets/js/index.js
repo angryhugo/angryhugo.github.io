ca8f5cdadc8ff0ffc1fc05a4c5a84831d407d256
yinhan <495552532@qq.com>
2017-09-26 08:57:58 +0800

    add desc+distance

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,65 @@
 $(function() {
     function initPic() {
+        var dateTime = new Date().getTime();
         var qiniuBaseUrl = "http://owntjivne.bkt.clouddn.com/";
         var $swiperWrap = $(".swiper-wrapper");
-        var dateDescArray = [
-            { 'date': '2017-08-28', 'des': '第一个七夕' },
-            { 'date': '2017-09-01', 'des': '居然停在一起' },
-            { 'date': '2017-09-03', 'des': '高中一样陪你做题' },
-            { 'date': '2017-09-09', 'des': '躺在阁楼的吊床上' },
-            { 'date': '2017-09-10', 'des': '去了个"假的"植物园' },
-            { 'date': '2017-09-16', 'des': '在家看我拍牌' },
-            { 'date': '2017-09-17', 'des': '说我拍照技术差' },
-            { 'date': '2017-09-23', 'des': '暗中观察爸爸' },
-            { 'date': '2017-09-24', 'des': '在地下车库抓狂 '}
+        var dateDescArray = [{
+                'date': '2017-08-28',
+                'desc': '第一个七夕',
+                'distance': '70'
+            },
+            {
+                'date': '2017-09-01',
+                'desc': '居然停在一起',
+                'distance': '73'
+            },
+            {
+                'date': '2017-09-03',
+                'desc': '高中一样陪你做题',
+                'distance': '73'
+            },
+            {
+                'date': '2017-09-09',
+                'desc': '躺在阁楼的吊床上',
+                'distance': '68'
+            },
+            {
+                'date': '2017-09-10',
+                'desc': '去了个"假的"植物园',
+                'distance': '108'
+            },
+            {
+                'date': '2017-09-16',
+                'desc': '在家看我拍牌',
+                'distance': '68'
+            },
+            {
+                'date': '2017-09-17',
+                'desc': '说我拍照技术差',
+                'distance': '68'
+            },
+            {
+                'date': '2017-09-23',
+                'desc': '暗中观察爸爸',
+                'distance': '68'
+            },
+            {
+                'date': '2017-09-24',
+                'desc': '在地下车库抓狂 ',
+                'distance': '85'
+            }
         ];
         for (var i = 0, len = dateDescArray.length; i < len; i++) {
             var dateDesc = dateDescArray[i];
             var date = dateDesc.date;
-            $swiperWrap.append('<div class="swiper-slide"><img src="' + qiniuBaseUrl + date + '.jpg" width="95%" class="swiper-lazy"><p class="swiper-desc">' + date + '</p></div>')
+            var desc = dateDesc.desc;
+            var distance = dateDesc.distance;
+            $swiperWrap.append('<div class="swiper-slide">' + '<p class="desc">' + desc + '</p><p class="date">' + date + '</p><p class="distance">' + distance + 'km</p><img src="' + qiniuBaseUrl + date + '.jpg?t="'+dateTime+' width="95%" class="swiper-lazy"></div>')
         }
     }
     initPic();
+    var width = $(window).width();
+    $(".swiper-container").css("height", width * 0.95 + 80);
     var swiper = new Swiper('.swiper-container', {
         // Enable lazy loading
         // lazyLoading: true,
